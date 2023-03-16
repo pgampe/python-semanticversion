ac7144c57a141019c87446d71e0591bbfd07dfcf
Philipp Gampe <philipp.gampe@student.hpi.de>
2023-03-16 10:53:26 +0000

    [FEATURE] Allow up to three leading zeros in parts

--- semantic_version/base.py
+++ semantic_version/base.py
@@ -1265,7 +1265,7 @@ class NpmSpec(BaseSpec):
         JOINER = '||'
         HYPHEN = ' - '
 
-        NUMBER = r'x|X|\*|0|[1-9][0-9]*'
+        NUMBER = r'x|X|\*|0|0{0,3}[1-9][0-9]*'
         PART = r'[a-zA-Z0-9.-]*'
         OP = r'~>|<=|>=|>|<|\^|~|='
         NPM_SPEC_BLOCK = re.compile(r"""
